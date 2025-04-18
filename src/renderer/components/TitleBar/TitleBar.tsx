a99042413563e513a9085fd817c7d06e7e774bb2
Mario C <mario.caval@icloud.com>
2025-04-18 21:49:26 +0100

    Added (visual only) terminal tabs on title bar

--- src/renderer/components/TitleBar/TitleBar.tsx
+++ src/renderer/components/TitleBar/TitleBar.tsx
@@ -1,12 +1,32 @@
 import { Button } from "../ui/button";
-import { Home } from "lucide-react";
+import { Home, Plus } from "lucide-react";
 import WindowControlBar from "../WindowControlBar";
+import CloseableTab from "../NavigationBar/CloseableTab";
+import { toast } from "sonner";
+
+const tabs = [
+  { name: "Lunar"},
+  { name: "Solar"},
+  { name: "Planetary"},
+]
 
 export default function TitleBar() {
+  const handleNewTab = () => {
+    toast.warning("New tab not implemented yet");
+  };
+
   return (
       <nav className="draggable flex justify-between select-none text-muted-foreground h-10">
         <div className="flex items-center space-x-2">
-        <Button className="cursor-pointer nonDraggable" variant="ghost" size="icon"><Home /></Button>
+          <Button className="cursor-pointer nonDraggable" variant="ghost" size="icon"><Home /></Button>
+          <div className="flex space-x-1">
+            {tabs.map((tab) => (
+              <CloseableTab key={tab.name} name={tab.name}/>
+            ))}
+          </div>
+          <Button variant="ghost" size="icon" className="cursor-pointer nonDraggable rounded-full size-8" onClick={handleNewTab}>
+            <Plus />
+          </Button>
         </div>
         <WindowControlBar />
       </nav>
