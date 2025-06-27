68dabe210426a6e20a398d4a416d8d0d2b463c78
Mario C <mario.caval@icloud.com>
2025-06-27 22:27:49 +0100

    Removed reference to local windowManager variable

--- src/main/main.ts
+++ src/main/main.ts
@@ -7,7 +7,6 @@ import log from 'electron-log/main';
 import { HostConfigManager } from './HostConfigManager';
 
 let terminalManager: TerminalManager;
-let windowManager: WindowManager;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
@@ -59,7 +58,7 @@ app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    windowManager.createMainWindow();
+    WindowManager.instance.createMainWindow();
     terminalManager.window = WindowManager.instance.mainWindow;
   }
 });
