4e4a55f28560849ce0392dab0b1866dccc4c72c1
Mario Caval <vcaval@cisco.com>
2025-06-28 15:30:55 +0100

    added vibrancy to main window config for macos

--- src/main/windowManager.ts
+++ src/main/windowManager.ts
@@ -33,8 +33,8 @@ export class WindowManager {
       ...windowConfig,
       minWidth: 500,
       minHeight: 500,
-      backgroundColor: 'rgba(10,10,10,0.5)',
       titleBarStyle: 'hidden',
+      vibrancy: 'hud',
       trafficLightPosition: { x: 10, y: 12 },
       webPreferences: {
         preload: path.join(__dirname, 'preload.js')
